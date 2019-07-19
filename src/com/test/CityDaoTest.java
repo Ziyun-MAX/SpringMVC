c913f4a0524d45e041cffe3b2c5b04a32726d11b
1670495543 <167049543@qq.com>
2019-07-19 14:41:31 +0800

    第二次修改

--- src/com/test/CityDaoTest.java
+++ src/com/test/CityDaoTest.java
@@ -17,6 +17,7 @@ public class CityDaoTest {
     public static void main(String[] args) {
         System.out.println("你好,世界!");
         System.out.println("hello world!");
+        System.out.println("孜云");
     }
 
 
